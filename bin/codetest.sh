c245471f73e0f89a7eecc15344c08eb528a1f79a
IritaSee <iga.narendra@gmail.com>
2024-10-02 17:15:07 +0900

    update automation for interidividual

--- bin/codetest.sh
+++ bin/codetest.sh
@@ -2,4 +2,4 @@ cd ..
 make clean all
 cd bin
 # ./drug_sim -input_deck input_deck_example.txt -hill_file mitoxantrone/mitoxantrone_100_samples_50_conc.csv -init_file init_mitoxantrone_100_samples_50_conc/_state_only.csv -cvar_file ./CVAR/HF_male.csv
-./drug_sim -input_deck input_deck_example.txt -hill_file mitoxantrone/mitoxantrone_100_samples_50_conc.csv -init_file init_mitoxantrone_100_samples_50_conc/_state_only.csv -cvar_file ./CVAR/HF_male.csv
+./drug_sim -input_deck input_deck_example.txt -hill_file mitoxantrone_control/mitoxantrone_100_samples_control.csv -init_file init_mitoxantrone_100_samples_control/_state_only.csv -cvar_file ./CVAR/HF_male_control.csv
